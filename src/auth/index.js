1dc9d0a0a3b089ace443151d847b706767e5190b
macjabeth <macbethjonathan@gmail.com>
2019-04-25 18:15:43 -0400

    update authentication to verify with access token

--- src/auth/index.js
+++ src/auth/index.js
@@ -4,12 +4,12 @@ import history from '../history';
 export default class Auth {
   accessToken;
   idToken;
-  expiresAt;
 
   auth0 = new auth0.WebAuth({
     domain: 'dev-d9y68pfa.auth0.com',
     clientID: 'k6xsJZMJxRo6xrlfua5hWIUS0Dms5w8G',
     redirectUri: 'http://localhost:3000/callback',
+    audience: 'https://lambda-trivializer.herokuapp.com/',
     responseType: 'token id_token',
     scope: 'openid'
   });
@@ -35,54 +35,30 @@ export default class Auth {
   getIdToken = () => this.idToken;
 
   setSession(authResult) {
-    // Set isLoggedIn flag in localStorage
-    localStorage.setItem('isLoggedIn', 'true');
+    // Set token flag in localStorage
+    localStorage.setItem('token', authResult.accessToken);
 
-    // Set the time that the access token will expire at
-    let expiresAt = authResult.expiresIn * 1000 + new Date().getTime();
     this.accessToken = authResult.accessToken;
     this.idToken = authResult.idToken;
-    this.expiresAt = expiresAt;
 
     // navigate to the home route
     history.replace('/home');
   }
 
-  renewSession = () => {
-    this.auth0.checkSession({}, (err, authResult) => {
-      if (authResult && authResult.accessToken && authResult.idToken) {
-        this.setSession(authResult);
-      } else if (err) {
-        this.logout();
-        console.log(err);
-        alert(
-          `Could not get a new token (${err.error}: ${err.error_description}).`
-        );
-      }
-    });
-  };
-
   logout = () => {
-    // Remove tokens and expiry time
+    // Remove tokens
     this.accessToken = null;
     this.idToken = null;
-    this.expiresAt = 0;
 
-    // Remove isLoggedIn flag from localStorage
-    localStorage.removeItem('isLoggedIn');
+    // Remove token flag from localStorage
+    localStorage.removeItem('token');
 
     this.auth0.logout({
       returnTo: window.location.origin
     });
-
-    // navigate to the home route
-    history.replace('/home');
   };
 
-  isAuthenticated = () => {
-    // Check whether the current time is past the
-    // access token's expiry time
-    let expiresAt = this.expiresAt;
-    return new Date().getTime() < expiresAt;
-  };
+  // Check whether the current time is past the
+  // access token's expiry time
+  isAuthenticated = () => this.accessToken || localStorage.getItem('token');
 }
