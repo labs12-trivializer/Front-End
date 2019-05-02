8388ce1c7e6476b70dc8b8d1c13296bfa13f50ad
macjabeth <macbethjonathan@gmail.com>
2019-05-02 16:56:30 -0400

    add profile reducer

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -3,13 +3,15 @@ import { combineReducers } from 'redux';
 import answers from './answers';
 import error from './error';
 import games, * as fromGames from './games';
+import profile from './profile';
 import rounds, * as fromRounds from './rounds';
 
 export default combineReducers({
-  games,
   answers,
-  rounds,
-  error
+  error,
+  games,
+  profile,
+  rounds
 });
 
 export const getAllGames = state =>
---
+++ src/reducers/profile.js
@@ -0,0 +1,16 @@
+import {
+  FETCH_PROFILE_SUCCESS,
+  ADD_PROFILE_SUCCESS,
+  EDIT_PROFILE_SUCCESS,
+} from '../actions/types';
+
+export default (state = {}, action) => {
+  switch (action.type) {
+    case FETCH_PROFILE_SUCCESS:
+    case ADD_PROFILE_SUCCESS:
+    case EDIT_PROFILE_SUCCESS:
+      return action.payload;
+    default:
+      return state;
+  }
+}
