995491bf950431a4aa9770478842d1a60d4a3b21
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-02 12:39:50 -0400

    refactor answers reducer
    This change was needed to handle normalized server responses.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,2 +1,9 @@
-export * from './answers';
 export * from './games';
+export * from './rounds';
+export * from './questions';
+export * from './answers';
+
+export const testAction = (profile) => ({
+  type: 'TEST_ACTION',
+  payload: profile
+});
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,19 +1,25 @@
 import { combineReducers } from 'redux';
 
-import answers from './answers';
+import answers, * as fromAnswers from './answers';
 import error from './error';
 import games, * as fromGames from './games';
 import rounds, * as fromRounds from './rounds';
+import questions, * as fromQuestions from './questions';
 
 export default combineReducers({
   games,
-  answers,
   rounds,
+  questions,
+  answers,
   error
 });
 
-export const getAllGames = state =>
-  fromGames.getAllGames(state.games)
+export const getAllGames = state => fromGames.getAllGames(state.games);
 
-export const getAllRounds = state =>
-  fromRounds.getAllRounds(state.rounds)
+export const getAllRounds = state => fromRounds.getAllRounds(state.rounds);
+
+export const getAllQuestions = state =>
+  fromQuestions.getAllQuestions(state.questions);
+
+export const getAllAnswers = state =>
+  fromAnswers.getAllAnswers(state.answers);
