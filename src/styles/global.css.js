a15f020f675475eb9e526dade238cae99b55ca4b
macjabeth <macbethjonathan@gmail.com>
2019-05-07 17:04:42 -0400

    add global css styling

---
+++ src/styles/global.css.js
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from 'styled-components';
+
+export default createGlobalStyle`
+  html {
+    font-size: 62.5%;
+  }
+
+  body {
+    font-family: 'Source Sans Pro', sans-serif;
+    font-size: 1.6rem;
+  }
+`;
