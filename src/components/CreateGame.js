fe19e917db62b8a4a7b4f2b0a57b30e723f1786b
Kory Newton <korybnewton@gmail.com>
2019-05-07 10:46:25 -0700

    brought state into CreateGame, storing info on requests to db

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -1,18 +1,83 @@
-import React, { Component } from 'react';
-import CreateRound from './CreateRound';
-import { connect } from 'react-redux';
-import { getAllCategories } from '../reducers';
+import React, { Component } from "react";
+import CreateRound from "./CreateRound";
+import { connect } from "react-redux";
+import { getAllCategories } from "../reducers";
+import serverHandshake from "../auth/serverHandshake";
 
 class CreateGame extends Component {
+  state = {
+    name: "Test Add New Game4",
+    game_id: null,
+    rounds: [],
+    numberOfRounds: 1,
+    questions: [],
+    answers: []
+  };
+
+  componentDidMount() {
+    // //create game in db
+    serverHandshake(true)
+      .post("/games", { name: this.state.name })
+      .then(res => {
+        console.log(res.data);
+        this.setState({ game_id: res.data.id });
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+    //make initial round in db
+    serverHandshake(true)
+      .post("./rounds", {
+        game_id: this.state.game_id,
+        number: this.state.numberOfRounds
+      })
+      .then(res => {
+        console.log("newly created round id:", res.data.id);
+        this.setState({
+          numberOfRounds: this.state.numberOfRounds + 1,
+          rounds: [...this.state.rounds, res.data.id]
+        });
+      })
+      .catch(err => console.log(err));
+  }
+
+  addRoundToDb() {
+    serverHandshake(true)
+      .post("/rounds", {
+        game_id: this.state.game_id,
+        number: this.state.numberOfRounds
+      })
+      .then(res => {
+        console.log(res.data);
+        this.setState({
+          numberOfRounds: this.state.numberOfRounds + 1,
+          rounds: [...this.state.rounds, res.data.id]
+        });
+      })
+      .catch(err => console.log(err));
+  }
 
   render() {
-    return (
-      <div>
-        <CreateRound categories={this.props.categories} />
-
-        {/* <AddRound /> */}
-      </div>
-    );
+    if (this.state.numberOfRounds === 1) {
+      return (
+        <>
+          <h3>Game: {this.state.name}</h3>
+          <CreateRound categories={this.props.categories} />
+          <button onClick={() => this.addRoundToDb()}>Add Another Round</button>
+        </>
+      );
+    } else {
+      return (
+        <>
+          <h3>Game: {this.state.name}</h3>
+          {this.state.rounds.map(item => (
+            <CreateRound categories={this.props.categories} key={item} />
+          ))}
+          <button onClick={() => this.addRoundToDb()}>Add Another Round</button>
+        </>
+      );
+    }
   }
 }
 
@@ -26,4 +91,3 @@ export default connect(
   mapStateToProps,
   null
 )(CreateGame);
-
