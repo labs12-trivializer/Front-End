67901bacf09af94243b7b0f541d38c64154b75f1
IVB107 <ianvb107@gmail.com>
2019-05-07 14:58:59 -0500

    Added functionality to create new round for a game

--- src/components/Game.js
+++ src/components/Game.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
+import { withRouter } from 'react-router-dom';
 // import { Link } from 'react-router-dom';
 
 import { fetchGame, addRound } from '../actions';
@@ -7,25 +8,25 @@ import Menu from './Menu';
 import Round from './Round';
 
 class Game extends Component {
-  // constructor(props) {
-  //   super(props);
-  // }
+  constructor(props) {
+    super(props);
+    this.state = {
+      rounds: this.props.game.rounds
+    }
+  }
 
   componentDidMount = () => {
     console.log('Game Info:', this.props.game);
     this.props.fetchGame(this.props.match.params.id);
   };
   
-  // check boolean state property 'showRoundForm'
-  handleShowForm = () => {
-    // set 'showRoundForm: true'
-  }
-  // Fire on submit of new round form
-  handleSubmitNewRound = () => {
-    this.props.addRound({
+  handleAddNewRound = async () => {
+    await this.props.addRound({
       "game_id": this.props.game.id,
       "number": `${this.props.game.rounds.length + 1}`
-    });
+    }).then(() => {
+      this.props.fetchGame(this.props.match.params.id);
+    })
   }
   
   render() {
@@ -48,7 +49,7 @@ class Game extends Component {
             )}
           </ul>
           <div>
-            <button onClick={this.handleSubmitNewRound}>New Round</button>
+            <button onClick={this.handleAddNewRound}>New Round</button>
           </div>
 
       </div>
@@ -65,6 +66,7 @@ export default connect(
   mapStateToProps,
   {
     fetchGame, 
-    addRound
+    addRound,
+    withRouter
   }
 )(Game);
