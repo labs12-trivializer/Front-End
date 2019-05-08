0d509062d83415f1ffc3f2dec8df762b93d69250
Kory Newton <korybnewton@gmail.com>
2019-05-08 10:35:44 -0700

    removed saveQuestionsToDb function

--- src/components/CreateGame.js
+++ src/components/CreateGame.js
@@ -61,40 +61,6 @@ class CreateGame extends Component {
       .catch(err => console.log(err));
   }
 
-  // saveQuestionsToDb(openTrivArray, round, user_id) {
-  //   let questionObj = {
-  //     round_id: round,
-  //     text: null,
-  //     user_id
-  //   };
-
-  //   let correctAnswerObj = {
-  //     question_id: null,
-  //     text: null,
-  //     is_correct: true
-  //   };
-
-  //   openTrivArray.forEach(obj => {
-  //     questionObj.text = obj.question;
-  //     correctAnswerObj.text = obj.correct_answer;
-
-  //     //server request to post question
-  //     serverHandshake(true)
-  //       .post("/questions", questionObj)
-  //       .then(res => {
-  //         console.log("question added successfully");
-  //         correctAnswerObj.question_id = res.data[0].id;
-
-  //         //server request to post correct answer using question ID in successful response
-  //         serverHandshake(true)
-  //           .post("/answers", correctAnswerObj)
-  //           .then(res => console.log(res.status))
-  //           .catch(err => console.log("error posting answer"));
-  //       })
-  //       .catch(err => console.log("error adding question"));
-  //   });
-  // }
-
   render() {
     if (this.state.nextRoundNumber < 1) {
       return (
@@ -112,7 +78,6 @@ class CreateGame extends Component {
               categories={this.props.categories}
               key={round_id}
               round_id={round_id}
-              // saveQuestionsToDb={this.saveQuestionsToDb}
               user_id={this.state.user_id}
               roundNumber={index + 1}
             />
