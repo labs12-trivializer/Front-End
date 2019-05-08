8c5fb5d4882ef0a19ff20f6e5b4b8c5c68495292
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-08 18:50:21 -0400

    add actions and reducers for questionTypes

---
+++ src/actions/questionTypes.js
@@ -0,0 +1,20 @@
+import {
+  FETCH_QUESTION_TYPES_START,
+  FETCH_QUESTION_TYPES_SUCCESS,
+  FETCH_QUESTION_TYPES_FAILURE,
+} from './types';
+
+import serverHandshake from '../auth/serverHandshake';
+
+export const fetchQuestionTypes = () => async dispatch => {
+  dispatch({ type: FETCH_QUESTION_TYPES_START });
+  try {
+    const success = await serverHandshake(true).get('/question_types/normalized');
+    console.log(success);
+    dispatch({ type: FETCH_QUESTION_TYPES_SUCCESS, payload: success.data });
+    return success;
+  } catch (error) {
+    dispatch({ type: FETCH_QUESTION_TYPES_FAILURE, payload: error });
+    return error;
+  }
+};
