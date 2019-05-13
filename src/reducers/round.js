6358256b724bc65ca9abff3403f174eef015fc34
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-12 23:00:50 -0400

    add reducer for drag/drop
    arrayMove came from: https://stackoverflow.com/questions/5306680/move-an-array-element-from-one-array-position-to-another

--- src/reducers/round.js
+++ src/reducers/round.js
@@ -3,8 +3,20 @@ import {
   DELETE_STATE_QUESTION,
   GET_NEW_ROUND_QUESTIONS_SUCCESS,
   ADD_CUSTOM_QUESTION,
-  ADD_QUESTION_SUCCESS
+  ADD_QUESTION_SUCCESS,
+  DRAG_DROP_QUESTION
 } from '../actions/types';
+
+const arrayMove = (arr, old_index, new_index) => {
+  if (new_index >= arr.length) {
+    var k = new_index - arr.length + 1;
+    while (k--) {
+      arr.push(undefined);
+    }
+  }
+  arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+  return arr; // for testing
+};
 
 const removeQuestion = (round, questionId) => {
   if (!round.questions || round.questions.length === 0) {
@@ -13,6 +25,15 @@ const removeQuestion = (round, questionId) => {
 
   return { ...round, questions: round.questions.filter(q => q !== questionId) };
 };
+
+const dragDropQuestion = (round, { dragIndex, hoverIndex }) => {
+  const newQuestions = arrayMove([...round.questions], dragIndex, hoverIndex);
+  return {
+    ...round,
+    questions: newQuestions
+  };
+};
+
 // reducer for a single round
 const round = (state, action) => {
   switch (action.type) {
@@ -40,6 +61,8 @@ const round = (state, action) => {
             ]
           }
         : { ...state, questions: [...state.questions, action.payload.result] };
+    case DRAG_DROP_QUESTION:
+      return dragDropQuestion(state, action.payload);
     default:
       return state;
   }
