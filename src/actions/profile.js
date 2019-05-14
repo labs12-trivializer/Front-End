64611aa9d2cee569c81ec261ff9899cbfb904652
Kory Newton <korybnewton@gmail.com>
2019-05-14 12:52:24 -0700

    upgrading of tier using redux actions

--- src/actions/profile.js
+++ src/actions/profile.js
@@ -8,7 +8,13 @@ import {
   ADD_PROFILE_FAILURE,
   EDIT_PROFILE_START,
   EDIT_PROFILE_SUCCESS,
-  EDIT_PROFILE_FAILURE
+  EDIT_PROFILE_FAILURE,
+  UPGRADE_TIER_START,
+  UPGRADE_TIER_SUCCESS,
+  UPGRADE_TIER_FAILURE,
+  CREATE_TOKEN_SUCESS,
+  SUBSCRIBE_CUSTOMER_SUCCESS,
+  CREATE_CUSTOMER_SUCCESS
 } from './types';
 
 import serverHandshake from '../auth/serverHandshake';
@@ -54,5 +60,35 @@ export const editProfile = profileData => async dispatch => {
   } catch (error) {
     dispatch({ type: EDIT_PROFILE_FAILURE, payload: error });
     return error;
+  }
+};
+
+export const upgradeTier = (plan, name, token) => async dispatch => {
+  dispatch({ type: UPGRADE_TIER_START });
+  try {
+    dispatch({ type: CREATE_TOKEN_SUCESS });
+
+    //create stripe customer using above token as their payment source
+    const customer = await serverHandshake(true).post('/billing/customer', {
+      name,
+      source: token.id //using the token returned above as their payment source
+    });
+    dispatch({ type: CREATE_CUSTOMER_SUCCESS });
+
+    //Using backend api, Subscribe customer to one of our two paid plans (silver or gold)
+    const subscribe = await serverHandshake(true).post('/billing/subscribe', {
+      customer: customer.data.id, //using customer id returned above.
+      plan //gold plan, silver plan: 'plan_Eyw8BcuV5qyAV2'
+    });
+
+    dispatch({
+      type: SUBSCRIBE_CUSTOMER_SUCCESS
+    });
+    dispatch({
+      type: UPGRADE_TIER_SUCCESS,
+      payload: subscribe.data.plan.nickname
+    });
+  } catch (error) {
+    dispatch({ type: UPGRADE_TIER_FAILURE });
   }
 };
--- src/components/stripe/CheckoutForm.js
+++ src/components/stripe/CheckoutForm.js
@@ -2,12 +2,12 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { CardElement, injectStripe } from 'react-stripe-elements';
 import CheckBox from './CheckBox';
-import serverHandshake from '../../auth/serverHandshake';
+import { upgradeTier } from '../../actions';
 
 class _CheckoutForm extends Component {
   constructor(props) {
     super(props);
-    this.submit = this.submit.bind(this);
+    // this.submit = this.submit.bind(this);
     this.state = {
       basicPlan: false,
       premiumPlan: false
@@ -19,9 +19,12 @@ class _CheckoutForm extends Component {
   togglePremiumPlan = e => {
     this.setState({ premiumPlan: !this.state.premiumPlan });
   };
-  //User hits submit after entering credit card info:
-  async submit(e) {
-    console.log(this.props.tier);
+
+  upgradeTier = async () => {
+    //create token from payment info
+    let { token } = await this.props.stripe.createToken({
+      name: this.props.profile.username
+    });
     const gold = 'plan_Eyw9DUPvzcFMvK';
     const silver = 'plan_Eyw8BcuV5qyAV2';
     let plan = null;
@@ -32,49 +35,63 @@ class _CheckoutForm extends Component {
     } else {
       return;
     }
-    // request to stripe API to tokenize credit card information, responds with token
-    //for compliance purposes we do not handle or store the customers credit card info, we use the tokenized version returned by Stripe
-    let { token } = await this.props.stripe.createToken({ name: 'Name' });
-    console.log(token.id);
+    this.props.upgradeTier(plan, this.props.profile.username, token);
+  };
+  // //User hits submit after entering credit card info:
+  // async submit(e) {
+  // const gold = 'plan_Eyw9DUPvzcFMvK';
+  // const silver = 'plan_Eyw8BcuV5qyAV2';
+  // let plan = null;
+  // if (this.state.premiumPlan && !this.state.basicPlan) {
+  //   plan = gold;
+  // } else if (this.state.basicPlan && !this.state.premiumPlan) {
+  //   plan = silver;
+  // } else {
+  //   return;
+  // }
+  //   // request to stripe API to tokenize credit card information, responds with token
+  //   //for compliance purposes we do not handle or store the customers credit card info, we use the tokenized version returned by Stripe
+  // let { token } = await this.props.stripe.createToken({ name: 'Name' });
+  //   console.log(token.id);
 
-    //create a new customer account associated with our app on Stripe
-    //this uses our backend API to communicate with Stripe
-    const customer = await serverHandshake(true).post('/billing/customer', {
-      name: 'hardcoded testname',
-      source: token.id //using the token returned above as their payment source
-    });
-    console.log('customer', customer);
+  //   //create a new customer account associated with our app on Stripe
+  //   //this uses our backend API to communicate with Stripe
+  //   const customer = await serverHandshake(true).post('/billing/customer', {
+  //     name: 'hardcoded testname',
+  //     source: token.id //using the token returned above as their payment source
+  //   });
+  //   console.log('customer', customer);
 
-    //Using backend api, Subscribe customer to one of our two paid plans (silver or gold)
+  //   //Using backend api, Subscribe customer to one of our two paid plans (silver or gold)
 
-    const subscribe = await serverHandshake(true).post('/billing/subscribe', {
-      customer: customer.data.id, //using customer id returned above.
-      plan //gold plan, silver plan: 'plan_Eyw8BcuV5qyAV2'
-    });
+  //   const subscribe = await serverHandshake(true).post('/billing/subscribe', {
+  //     customer: customer.data.id, //using customer id returned above.
+  //     plan //gold plan, silver plan: 'plan_Eyw8BcuV5qyAV2'
+  //   });
 
-    console.log(subscribe.status);
-    if (subscribe.status === 200) console.log('Purchase Complete!');
-  }
+  //   console.log(subscribe.status);
+  //   if (subscribe.status === 200) console.log('Purchase Complete!');
+  // }
 
   render() {
-    if (this.props.tier !== 'gold') {
+    if (this.props.profile.tier_name !== 'gold') {
       return (
         <div className="checkout">
-          <h2>Current Tier: {this.props.tier}</h2>
+          <h2>Current Tier: {this.props.profile.tier_name}</h2>
           <p>Payment Info:</p>
           <CardElement />
           <CheckBox
             toggleBasic={this.toggleBasicPlan}
             togglePremium={this.togglePremiumPlan}
-            tier={this.props.tier}
+            tier={this.props.profile.tier_name}
           />
-          <button onClick={this.submit}>Send</button>
+          <button onClick={this.upgradeTier}>Send</button>
         </div>
       );
     } else {
       return (
         <div>
-          <h2>Current Tier: {this.props.tier}</h2>
+          <h2>Current Tier: {this.props.profile.tier_name}</h2>
           <h1>You are already subscribed to the highest tier plan</h1>
         </div>
       );
@@ -85,10 +102,10 @@ class _CheckoutForm extends Component {
 const CheckoutForm = injectStripe(_CheckoutForm);
 
 const mapStateToProps = state => ({
-  tier: state.profile.tier_name
+  profile: state.profile
 });
 
 export default connect(
   mapStateToProps,
-  {}
+  { upgradeTier }
 )(CheckoutForm);
