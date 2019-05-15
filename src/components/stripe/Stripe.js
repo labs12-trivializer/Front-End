3dc35c2c8d4f946dbd4a2cf94f81cf39ee07164d
Kory Newton <korybnewton@gmail.com>
2019-05-14 17:25:28 -0700

    added global background to billing page

--- src/components/stripe/Stripe.js
+++ src/components/stripe/Stripe.js
@@ -2,16 +2,19 @@ import React from 'react';
 import CheckoutForm from './CheckoutForm';
 import { Elements, StripeProvider } from 'react-stripe-elements';
 import waitForProfile from '../waitForProfile';
+import { Background } from '../../styles/shared.css';
 
 const Stripe = () => {
   return (
-    <div>
+    <>
+      <Background />
       <StripeProvider apiKey="pk_test_rLIPiZV9cJfPy9p4WZgEMCbA00qbhu5zTZ">
         <Elements>
           <CheckoutForm />
         </Elements>
       </StripeProvider>
-    </div>
+      <Background />
+    </>
   );
 };
 
