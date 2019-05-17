e36ffb5c36c73e0eeb0cd576fb38c88563e37fee
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-17 16:16:04 -0400

    update login callback page to have background and loading spinner

--- src/components/Callback.js
+++ src/components/Callback.js
@@ -1,11 +1,18 @@
-import React from 'react'
+import React from 'react';
+import Spinner from 'react-spinkit';
+
+import { Center } from '../styles/callback.css';
+import { Background } from '../styles/shared.css';
 
 const Callback = () => {
   return (
-    <div>
-      Loading...
-    </div>
-  )
-}
+    <>
+      <Background />
+      <Center>
+        <Spinner name="ball-scale-ripple" color="#25E1D2" />
+      </Center>
+    </>
+  );
+};
 
-export default Callback
+export default Callback;
