1d18bba60132fdecaff5a673bea8107910dc5e06
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-18 16:14:55 -0400

    account for ngrok for the backend url

--- src/auth/serverHandshake.js
+++ src/auth/serverHandshake.js
@@ -1,9 +1,23 @@
 import axios from 'axios';
 import { store } from '../store';
 
+const getBackendUrl = () => {
+  const url = window.location.href;
+  const split = url.split('/');
+
+  if (split[2].indexOf('ngrok') > -1) {
+    return 'https://lambda-trivializer.herokuapp.com/api';
+  }
+
+  return null;
+};
+
 const serverHandshake = auth => {
   const options = {
-    baseURL: process.env.REACT_APP_BACKEND_URI || 'http://localhost:9000/api'
+    baseURL:
+      process.env.REACT_APP_BACKEND_URI ||
+      getBackendUrl() ||
+      'http://localhost:9000/api'
     // baseURL: 'https://lambda-trivializer.herokuapp.com/api' || 'http://localhost:9000/api'
   };
 
