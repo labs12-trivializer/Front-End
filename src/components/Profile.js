414156f57e78663aab5cbb27bfb8fcfb1decd85a
IVB107 <ianvb107@gmail.com>
2019-05-20 13:07:12 -0500

    Added Material-ui form styling to Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -2,13 +2,49 @@ import React, { useEffect } from 'react';
 import { connect } from 'react-redux';
 import { Image } from 'cloudinary-react';
 import Tilt from 'react-tilt';
+import { 
+  FormControl, 
+  TextField, 
+  Button,
+  Typography
+} from '@material-ui/core';
+import { makeStyles } from '@material-ui/styles';
 
-import { Container, Form, Avatar } from '../styles/profile.css';
-import { Background, Button } from '../styles/shared.css';
+// import { Container, Form, Avatar } from '../styles/profile.css';
+// import { Background, Button } from '../styles/shared.css';
 
 import { fetchProfile, editProfile } from '../actions';
 
+const useStyles = makeStyles(theme => ({
+  container: {
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    // height: '100vh',
+    padding: 0,
+    margin: 0
+  },
+  form: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    // marginBottom: '.5rem'
+  },
+  formControl: {
+    width: '80%'
+  },
+  button: {
+    margin: '.5rem 0',
+    alignSelf: 'flex-start'
+  }
+}));
+
 const Profile = ({ profile, fetchProfile, editProfile }) => {
+
+  const classes = useStyles();
+
   useEffect(() => {
     console.log('avatarid:', profile.avatar_id);
     if (!profile.avatar_id) fetchProfile();
@@ -31,10 +67,10 @@ const Profile = ({ profile, fetchProfile, editProfile }) => {
   };
 
   return (
-    <Container>
-      <Background />
+    <div className={classes.container}>
+      {/* <Background /> */}
       <Tilt className="Tilt" options={{ max: 30 }}>
-        <Avatar onClick={displayWidget} className="Tilt-inner" avatar={profile.avatar_id}>
+        <figure onClick={displayWidget} className="Tilt-inner" avatar={profile.avatar_id}>
           {profile.avatar_id ? (
             <Image cloudName="trivializer" publicId={profile.avatar_id} />
           ) : (
@@ -43,28 +79,64 @@ const Profile = ({ profile, fetchProfile, editProfile }) => {
           <div className="middle">
             <span className="text">Upload Photo</span>
           </div>
-        </Avatar>
+        </figure>
       </Tilt>
-      <Form>
-        <div>
-          <label htmlFor="profile-name">Name</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="profile-email">Email</label>
-          <input type="text" />
-        </div>
-        <div>
-          <label htmlFor="profile-oldpass">Old Password</label>
-          <input type="password" />
-        </div>
-        <div>
-          <label htmlFor="profile-newpass">New Password</label>
-          <input type="password" />
-        </div>
-        <Button>Save</Button>
-      </Form>
-    </Container>
+      <form className={classes.form}>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="filled-uncontrolled"
+            label="First Name"
+            // defaultValue="Name"
+            margin="dense"
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="filled-uncontrolled"
+            label="Last Name"
+            // defaultValue="Name"
+            margin="dense"
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="filled-uncontrolled"
+            required
+            label="Display Name"
+            // defaultValue="Name"
+            margin="dense"
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="outlined-dense"
+            label="Email"
+            margin="dense"
+            variant="outlined"
+          />
+        </FormControl>
+        <FormControl className={classes.formControl}>
+          <TextField
+            id="outlined-dense"
+            label="Password"
+            margin="dense"
+            variant="outlined"
+          />
+        </FormControl>
+        <Typography variant="caption" color="inherit">*Required Field</Typography>
+        <FormControl className={classes.formControl}>
+          <Button 
+            className={classes.button}
+            variant="outlined" 
+            color="primary">
+            Save
+          </Button>
+        </FormControl>
+      </form>
+    </div>
   );
 };
 
