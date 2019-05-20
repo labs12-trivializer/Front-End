ad0c68dcfd5b007a2dc0b0337346cb8478840694
IVB107 <ianvb107@gmail.com>
2019-05-20 18:32:26 -0500

    Added additional styling & functionality to Profile.js

--- src/components/Profile.js
+++ src/components/Profile.js
@@ -3,10 +3,14 @@ import { connect } from 'react-redux';
 import { Image } from 'cloudinary-react';
 import Tilt from 'react-tilt';
 import { 
+  Paper,
   FormControl, 
   TextField, 
   Button,
-  Typography
+  Typography,
+  List,
+  ListItem,
+  ListItemText
 } from '@material-ui/core';
 import { makeStyles } from '@material-ui/styles';
 
@@ -37,13 +41,44 @@ const useStyles = makeStyles(theme => ({
   },
   button: {
     margin: '.5rem 0',
-    alignSelf: 'flex-start'
+    // alignSelf: 'flex-start'
+  },
+  paper: {
+    width: '100%',
+    padding: '1rem 0',
+    margin: '0 0 1rem'
+  },
+  avatarPaper: {
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    width: '100%',
+    padding: '.5rem 0',
+    margin: '0 0 1rem'
+  },
+  typography: {
+    width: '80%',
+    alignSelf: 'center'
   }
 }));
 
 const Profile = ({ profile, fetchProfile, editProfile }) => {
 
   const classes = useStyles();
+  const [values, setValues] = React.useState({
+    first_name: 'test',
+    last_name: 'test',
+    display_name: 'test',
+    email: 'test'
+  });
+
+  const handleChange = prop => event => {
+    setValues({ ...values, [prop]: event.target.value });
+  };
+
+  const handleSaveInfo = async () => {
+    console.log('PROFILE: ', await fetchProfile());
+  }
 
   useEffect(() => {
     console.log('avatarid:', profile.avatar_id);
@@ -66,76 +101,111 @@ const Profile = ({ profile, fetchProfile, editProfile }) => {
     widget.open();
   };
 
+  console.log('VALUES: ', Object.keys(values))
+  let isEditing = false;
+
   return (
     <div className={classes.container}>
-      {/* <Background /> */}
-      <Tilt className="Tilt" options={{ max: 30 }}>
-        <figure onClick={displayWidget} className="Tilt-inner" avatar={profile.avatar_id}>
-          {profile.avatar_id ? (
-            <Image cloudName="trivializer" publicId={profile.avatar_id} />
-          ) : (
-            <img src="https://picsum.photos/100" alt="placeholder" />
-          )}
-          <div className="middle">
-            <span className="text">Upload Photo</span>
-          </div>
-        </figure>
-      </Tilt>
-      <form className={classes.form}>
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="filled-uncontrolled"
-            label="First Name"
-            // defaultValue="Name"
-            margin="dense"
-            variant="outlined"
-          />
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="filled-uncontrolled"
-            label="Last Name"
-            // defaultValue="Name"
-            margin="dense"
-            variant="outlined"
-          />
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="filled-uncontrolled"
-            required
-            label="Display Name"
-            // defaultValue="Name"
-            margin="dense"
-            variant="outlined"
-          />
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="outlined-dense"
-            label="Email"
-            margin="dense"
-            variant="outlined"
-          />
-        </FormControl>
-        <FormControl className={classes.formControl}>
-          <TextField
-            id="outlined-dense"
-            label="Password"
-            margin="dense"
-            variant="outlined"
-          />
-        </FormControl>
-        <Typography variant="caption" color="inherit">*Required Field</Typography>
-        <FormControl className={classes.formControl}>
+      <Paper className={classes.avatarPaper}>
+        <Tilt className="Tilt" options={{ max: 30 }}>
+          <figure onClick={displayWidget} className="Tilt-inner" avatar={profile.avatar_id}>
+            {profile.avatar_id ? (
+              <Image cloudName="trivializer" publicId={profile.avatar_id} />
+            ) : (
+              <img src="https://picsum.photos/100" alt="placeholder" />
+            )}
+            <div className="middle">
+              <span className="text">Upload Photo</span>
+            </div>
+          </figure>
+        </Tilt>
+        <Typography
+          variant="h5"
+        >
+          Tier Level: {`${(profile.tier_name.toUpperCase())}`}
+        </Typography>
+        {profile.tier_name !== 'bronze' &&
           <Button 
+            variant="contained" 
+            href="/billing" 
             className={classes.button}
-            variant="outlined" 
-            color="primary">
-            Save
+            color="primary"
+          >
+            Upgrade Your Account
           </Button>
-        </FormControl>
-      </form>
+        }
+      </Paper>
+      {isEditing 
+        ? (
+          <List>
+            {Object.keys(values).map((value, idx) => 
+              <ListItem>
+                <ListItemText
+                  primary={values[value]}
+                  key={idx}
+                />
+              </ListItem>
+            )}
+          </List>
+        ) 
+        : (
+          <Paper className={classes.paper}>
+            <form className={classes.form}>
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="filled-uncontrolled"
+                label="First Name"
+                value={values.first_name}
+                onChange={handleChange('first_name')}
+                margin="dense"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="filled-uncontrolled"
+                label="Last Name"
+                value={values.last_name}
+                onChange={handleChange('last_name')}
+                margin="dense"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="filled-uncontrolled"
+                label="Display Name"
+                value={values.display_name}
+                onChange={handleChange('display_name')}
+                defaultValue={profile.username}
+                margin="dense"
+                variant="outlined"
+              />
+            </FormControl>
+            <FormControl className={classes.formControl}>
+              <TextField
+                id="outlined-dense"
+                required
+                label="Email"
+                value={values.email}
+                onChange={handleChange('email')}
+                margin="dense"
+                variant="outlined"
+              />
+            </FormControl>
+            {/* <Typography variant="caption" color="inherit">*Required Field</Typography> */}
+            <FormControl className={classes.formControl}>
+              <Button 
+                onClick={handleSaveInfo}
+                className={classes.button}
+                variant="outlined" 
+                color="primary">
+                Save Profile
+              </Button>
+            </FormControl>
+          </form>
+        </Paper>
+        )}
     </div>
   );
 };
