9f9b992dac5a57a953489620c6f52911fa458a4b
heyjuststart <41658450+heyjuststart@users.noreply.github.com>
2019-05-21 18:57:15 -0400

    restore redux-logger and sentry

--- src/store.js
+++ src/store.js
@@ -1,11 +1,11 @@
-import { createStore, applyMiddleware } from 'redux';
-// import { createStore, applyMiddleware, compose } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+// import { createStore, applyMiddleware } from 'redux';
+import { createStore, applyMiddleware, compose } from 'redux';
+// import { composeWithDevTools } from 'redux-devtools-extension';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
-// import { crashReporter } from './services/sentry';
+import { crashReporter } from './services/sentry';
 import thunk from 'redux-thunk';
-// import logger from 'redux-logger';
+import logger from 'redux-logger';
 import rootReducer from './reducers';
 
 const persistConfig = {
@@ -18,8 +18,8 @@ const persistedReducer = persistReducer(persistConfig, rootReducer);
 const configureStore = () => {
   const store = createStore(
     persistedReducer,
-    // compose(applyMiddleware(thunk, logger, crashReporter))
-    composeWithDevTools(applyMiddleware(thunk))
+    compose(applyMiddleware(thunk, logger, crashReporter))
+    // composeWithDevTools(applyMiddleware(thunk))
   );
 
   const persistor = persistStore(store);
