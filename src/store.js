1f3399037b1d7da3e4eb23f51809594210e3caf3
Jonathan Picazo <macbethjonathan@gmail.com>
2019-05-24 12:24:34 -0400

    only use redux devtools for development

--- src/store.js
+++ src/store.js
@@ -1,5 +1,5 @@
 import { createStore, applyMiddleware } from 'redux';
-import { composeWithDevTools } from 'redux-devtools-extension';
+import { composeWithDevTools } from 'redux-devtools-extension/developmentOnly';
 import { persistStore, persistReducer } from 'redux-persist';
 import storage from 'redux-persist/lib/storage';
 import { crashReporter } from './services/sentry';
